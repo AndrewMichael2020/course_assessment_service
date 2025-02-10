9f3b605a6fccbcf5addee6896b8508d09be49a1d
Andrew Michael <burnaby_now@icloud.com>
2025-02-10 14:57:35 -0500

    added exam to models.py and admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,41 @@
-from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from django.contrib import admin                # Import Django's admin module to customize the admin interface
+# <HINT> Import any new Models here             # Developer note: import additional models if needed
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission  # Import specified models from the local models module
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# <HINT> Register QuestionInline and ChoiceInline classes here  # Developer note: register inline admin classes for Question and Choice
 
+class LessonInline(admin.StackedInline):        # Define an inline admin class for the Lesson model using a stacked layout
+    model = Lesson                              # Set Lesson as the model for inline editing
+    extra = 5                                   # Display 5 extra blank Lesson forms in the admin interface
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+# Register your models here.                    # Comment indicating the start of model registration with the admin site
+class CourseAdmin(admin.ModelAdmin):            # Define a custom admin class for the Course model
+    inlines = [LessonInline]                    # Include inline Lesson forms on the Course admin page
+    list_display = ('name', 'pub_date')         # Display 'name' and 'pub_date' columns in the Course list view
+    list_filter = ['pub_date']                  # Add a filter sidebar for 'pub_date' in the Course admin
+    search_fields = ['name', 'description']     # Enable search on 'name' and 'description' fields in the Course admin
 
+class LessonAdmin(admin.ModelAdmin):            # Define a custom admin class for the Lesson model
+    list_display = ['title']                    # Display the 'title' column in the Lesson list view
 
-# Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+# <HINT> Register Question and Choice models here  # Developer note: register admin classes for Question and Choice
 
+class ChoiceInline(admin.StackedInline):         # Define an inline admin class for the Choice model using a stacked layout
+    model = Choice                              # Set Choice as the model for inline editing
+    extra = 2                                   # Display 2 extra blank Choice forms in the admin interface
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+class QuestionInline(admin.StackedInline):       # Define an inline admin class for the Question model using a stacked layout
+    model = Question                            # Set Question as the model for inline editing
+    extra = 2                                   # Display 2 extra blank Question forms in the admin interface
 
+class QuestionAdmin(admin.ModelAdmin):           # Define a custom admin class for the Question model
+    inlines = [ChoiceInline]                    # Include inline Choice forms on the Question admin page
+    list_display = ['content']                  # Display the 'content' column in the Question list view
 
-# <HINT> Register Question and Choice models here
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
+admin.site.register(Course, CourseAdmin)         # Register the Course model with the admin site using CourseAdmin
+admin.site.register(Lesson, LessonAdmin)         # Register the Lesson model with the admin site using LessonAdmin
+admin.site.register(Instructor)                  # Register the Instructor model with default admin options
+admin.site.register(Learner)                     # Register the Learner model with default admin options
+admin.site.register(Question, QuestionAdmin)     # Register the Question model with the admin site using QuestionAdmin
+admin.site.register(Choice)                      # Register the Choice model with default admin options
+admin.site.register(Submission)                  # Register the Submission model with default admin options
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,103 +1,123 @@
-import sys
-from django.utils.timezone import now
+import sys                                      # Provides system-specific functions and parameters
+from django.utils.timezone import now           # Imports the current time function from Django's timezone utilities
 try:
-    from django.db import models
+    from django.db import models                # Attempts to import Django's ORM models module for defining database models
 except Exception:
-    print("There was an error loading django modules. Do you have django installed?")
-    sys.exit()
+    print("There was an error loading django modules. Do you have django installed?")  # Prints error if Django isn't installed or fails to load
+    sys.exit()                                  # Exits the program if the import fails
 
-from django.conf import settings
-import uuid
-
+from django.conf import settings                  # Imports Django's settings to access configuration (e.g., AUTH_USER_MODEL)
+import uuid                                       # Imports the UUID module for generating unique identifiers (not used in this snippet)
 
 # Instructor model
-class Instructor(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+class Instructor(models.Model):                   # Defines the Instructor model inheriting from Django's base model
+    user = models.ForeignKey(                    # Creates a foreign key linking each instructor to a user account
+        settings.AUTH_USER_MODEL,                # References the user model defined in Django settings
+        on_delete=models.CASCADE,                # Deletes the instructor record if the associated user is deleted
     )
-    full_time = models.BooleanField(default=True)
-    total_learners = models.IntegerField()
+    full_time = models.BooleanField(default=True) # Boolean field indicating if the instructor is full-time (defaults to True)
+    total_learners = models.IntegerField()         # Integer field to store the total number of learners taught
 
-    def __str__(self):
-        return self.user.username
-
+    def __str__(self):                             # Method to define the string representation of an Instructor instance
+        return self.user.username                  # Returns the username of the associated user
 
 # Learner model
-class Learner(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+class Learner(models.Model):                      # Defines the Learner model
+    user = models.ForeignKey(                    # Creates a foreign key linking each learner to a user account
+        settings.AUTH_USER_MODEL,                # References the user model from Django settings
+        on_delete=models.CASCADE,                # Deletes the learner record if the associated user is deleted
     )
-    STUDENT = 'student'
-    DEVELOPER = 'developer'
-    DATA_SCIENTIST = 'data_scientist'
-    DATABASE_ADMIN = 'dba'
-    OCCUPATION_CHOICES = [
+    STUDENT = 'student'                           # Constant representing a student occupation
+    DEVELOPER = 'developer'                       # Constant representing a developer occupation
+    DATA_SCIENTIST = 'data_scientist'               # Constant representing a data scientist occupation
+    DATABASE_ADMIN = 'dba'                        # Constant representing a database admin occupation
+    OCCUPATION_CHOICES = [                        # List of tuples defining available occupation choices
         (STUDENT, 'Student'),
         (DEVELOPER, 'Developer'),
         (DATA_SCIENTIST, 'Data Scientist'),
         (DATABASE_ADMIN, 'Database Admin')
     ]
-    occupation = models.CharField(
+    occupation = models.CharField(               # Character field to store the occupation with restricted choices
         null=False,
         max_length=20,
-        choices=OCCUPATION_CHOICES,
-        default=STUDENT
+        choices=OCCUPATION_CHOICES,               # Restricts the field to predefined occupation choices
+        default=STUDENT                           # Sets default occupation to 'student'
     )
-    social_link = models.URLField(max_length=200)
+    social_link = models.URLField(max_length=200)  # URL field to store a link to the learner's social profile
 
-    def __str__(self):
-        return self.user.username + "," + \
-               self.occupation
-
+    def __str__(self):                             # Defines the string representation of a Learner instance
+        return self.user.username + "," + self.occupation  # Returns a string combining username and occupation
 
 # Course model
-class Course(models.Model):
-    name = models.CharField(null=False, max_length=30, default='online course')
-    image = models.ImageField(upload_to='course_images/')
-    description = models.CharField(max_length=1000)
-    pub_date = models.DateField(null=True)
-    instructors = models.ManyToManyField(Instructor)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
-    total_enrollment = models.IntegerField(default=0)
-    is_enrolled = False
+class Course(models.Model):                       # Defines the Course model
+    name = models.CharField(null=False, max_length=30, default='online course')  # Char field for course name with a default value
+    image = models.ImageField(upload_to='course_images/')  # Image field for the course, saving files to the 'course_images/' directory
+    description = models.CharField(max_length=1000)  # Char field for course description
+    pub_date = models.DateField(null=True)         # Date field for the publication date; can be null
+    instructors = models.ManyToManyField(Instructor)  # Many-to-many relationship with the Instructor model
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')  # Many-to-many relationship with User model via the Enrollment model
+    total_enrollment = models.IntegerField(default=0)  # Integer field to count total enrollments, defaulting to 0
+    is_enrolled = False                           # Non-model attribute (plain Python attribute) indicating enrollment status
 
-    def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
-
+    def __str__(self):                             # Defines the string representation of a Course instance
+        return "Name: " + self.name + "," + "Description: " + self.description  # Returns a string with course name and description
 
 # Lesson model
-class Lesson(models.Model):
-    title = models.CharField(max_length=200, default="title")
-    order = models.IntegerField(default=0)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    content = models.TextField()
-
+class Lesson(models.Model):                       # Defines the Lesson model
+    title = models.CharField(max_length=200, default="title")  # Char field for the lesson title with a default value
+    order = models.IntegerField(default=0)         # Integer field to specify the order of lessons, defaulting to 0
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # Foreign key linking a lesson to a course; cascade deletion applies
+    content = models.TextField()                   # Text field for the lesson content
 
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
-class Enrollment(models.Model):
-    AUDIT = 'audit'
-    HONOR = 'honor'
-    BETA = 'BETA'
-    COURSE_MODES = [
+class Enrollment(models.Model):                   # Defines the Enrollment model to track course enrollments
+    AUDIT = 'audit'                             # Constant representing audit mode enrollment
+    HONOR = 'honor'                             # Constant representing honor mode enrollment
+    BETA = 'BETA'                               # Constant representing beta mode enrollment
+    COURSE_MODES = [                            # List of tuples defining enrollment mode choices
         (AUDIT, 'Audit'),
         (HONOR, 'Honor'),
         (BETA, 'BETA')
     ]
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    date_enrolled = models.DateField(default=now)
-    mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
-    rating = models.FloatField(default=5.0)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Foreign key linking to a User; cascade deletion applies
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # Foreign key linking to a Course; cascade deletion applies
+    date_enrolled = models.DateField(default=now)  # Date field recording when the enrollment occurred; defaults to current time
+    mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)  # Char field for enrollment mode, limited to predefined choices with a default of audit
+    rating = models.FloatField(default=5.0)         # Float field for course rating, defaulting to 5.0
 
+class Question(models.Model):                     # Defines the Question model for assessments
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # Foreign key linking a question to a course; cascade deletion applies
+    content = models.CharField(max_length=200)      # Char field for the question content
+    grade = models.IntegerField(default=50)         # Integer field for the grade/weight of the question, defaulting to 50
 
-# One enrollment could have multiple submission
+    # method to calculate if the learner gets the score of the question
+    def __str__(self):                            # String representation method for Question
+        return "Question: " + self.content         # Returns the question content prefixed with "Question: "
+
+    def is_get_score(self, selected_ids):         # Method to determine if the learner's selected choices yield full marks
+        all_answers = self.choice_set.filter(is_correct=True).count()  # Counts all correct choices for this question
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()  # Counts correct choices selected by the learner
+        if all_answers == selected_correct:         # If learner selected all correct choices...
+            return True                             # ...return True indicating full score
+        else:
+            return False                            # Otherwise, return False
+
+class Choice(models.Model):                       # Defines the Choice model for each question option
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # Foreign key linking a choice to a question; cascade deletion applies
+    content = models.CharField(max_length=200)      # Char field for the content of the choice
+    is_correct = models.BooleanField(default=False) # Boolean field indicating if the choice is correct; defaults to False
+
+class Submission(models.Model):                   # Defines the Submission model to record exam submissions
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)  # Foreign key linking submission to an enrollment record; cascade deletion applies
+    choices = models.ManyToManyField(Choice)       # Many-to-many field linking submission to selected choices
+
+# Comments below explain potential alternative design or additional notes:
+# One enrollment could have multiple submissions
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
+# The commented out Submission model below is an alternate (redundant) definition:
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
